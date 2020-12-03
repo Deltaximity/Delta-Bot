4d49858476055efa92105fb70d3c100ba809d1f3
ItzHazzan <45205838+ItzHazzan@users.noreply.github.com>
2020-12-03 20:56:48 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,11 +4,11 @@ module.exports = {
     async execute(message, args) {
         const amount = args.join(' '); // Amount of messages which should be deleted
 
-        if (!amount) return message.reply("You haven't given an amount of messages which should be deleted!"); // Check if the 'amount' parameter is given
-        if (isNaN(amount)) return message.reply("The amount must be a number!"); // Check if the 'amount' parameter is a number. If not, the command throws an error
+        if (!amount) return message.reply("I'm sorry, but you can't delete `air`."); // Check if the 'amount' parameter is given
+        if (isNaN(amount)) return message.reply("I won't even try to count *that* lol."); // Check if the 'amount' parameter is a number. If not, the command throws an error
 
-        if (amount > 100) return message.reply("You can't delete more than 100 messages at once!"); // Checks if the `amount` integer is bigger than 100
-        if (amount < 1) return message.reply("You have to delete at least 1 message!"); // Checks if the `amount` integer is smaller than 1
+        if (amount > 100) return message.reply("Did I mention that the trunk can only hold 100 messages?"); // Checks if the `amount` integer is bigger than 100
+        if (amount < 1) return message.reply("Nice try, but it won't work on me."); // Checks if the `amount` integer is smaller than 1
 
         await message.channel.messages.fetch({ limit: amount}).then(messages => { // Fetches the messages
             message.channel.bulkDelete(messages); // Bulk deletes messages
